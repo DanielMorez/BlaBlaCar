d329854675ae3e6eebcf8a7a99ccf3c62cb3600c
DanielMorez <denisduginov17@gmail.com>
2020-12-15 00:55:34 +0400

    Добавление поста и отображение на главной

---
+++ Controllers/RouteController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlaBlaCar.Domain.DB;
+using BlaBlaCar.Domain;
+using BlaBlaCar.Security.Extensions;
+using BlaBlaCar.ViewModels.Route;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using BlaBlaCar.ViewModels.Route;
+
+namespace BlaBlaCar.Controllers
+{
+    /// <summary>
+    /// Контроллер для работы с блогом
+    /// </summary>
+    public class RouteController : Controller
+    {
+        private readonly RouteDbContext _routeDbContext;
+
+        public RouteController(RouteDbContext routeDbContext)
+        {
+            _routeDbContext = routeDbContext ?? throw new ArgumentNullException(nameof(routeDbContext));
+        }
+
+
+        /// <summary>
+        /// Метод открывает страницу с добавлением поста
+        /// </summary>
+        /// <returns>Возварщает страницу для добавления поста</returns>
+        [HttpGet]
+        public IActionResult AddRoute()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Добавление поста
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>Переход на страницу постов пользователя</returns>
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddRoute(NewRouteViewModels model)
+        {
+
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = this.GetAuthorizedUser();
+
+            Console.WriteLine(model);
+
+            var post = new Route
+            {
+                Driver = user.Employee,
+                Car = model.Car,
+                ToCity = model.ToCity,
+                FromCity = model.FromCity,
+                Price = model.Price,
+                Date = model.Date,
+                PassengersAmount = model.PassengersAmount
+            };
+
+            _routeDbContext.Routes.Add(post);
+
+            _routeDbContext.SaveChanges();
+
+            return View();
+
+        }
+
+        /// <summary>
+        /// Возвращение представления Blog возвратом Index.cshtml
+        /// </summary>
+        /// <returns>View</returns>
+        /*public IActionResult Index()
+        {
+            var posts = _routeDbContext.Routes
+                .Select(x => new ShowAllRouteViewModel
+                {
+                    Author = x.Owner.FullName,
+                    Date = x.Created,
+                    Data = x.Data,
+                    Title = x.Title
+                }).OrderByDescending(x => x.Date);
+
+            return View(posts);
+        }*/
+    }
+}
---
+++ ViewModels/Route/NewRouteViewModels.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlaBlaCar.ViewModels.Route
+{
+    public class NewRouteViewModels
+    {
+
+        /// <summary>
+        /// Город выезда
+        /// </summary>
+        [Required]
+        [Display(Name = "Город вызда")]
+        public String FromCity { get; set; }
+
+        /// <summary>
+        /// Город приезда
+        /// </summary>
+        [Required]
+        [Display(Name = "Город назначения ")]
+        public String ToCity { get; set; }
+
+        /// <summary>
+        /// Дата выезда
+        /// </summary>
+        [Required]
+        [Display(Name = "Дата выезда")]
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Цена за пассажира
+        /// </summary>
+        [Required]
+        [Display(Name = "Цена за пассажира")]
+        public int Price { get; set; }
+
+        /// <summary>
+        /// Машина
+        /// </summary>
+        [Required]
+        [Display(Name = "Машина")]
+        public String Car{ get; set; }
+        /// <summary>
+        /// Количесество пассажиров
+        /// </summary>
+        [Required]
+        [Display(Name = "Количество пассажиров")]
+        public int PassengersAmount { get; set; }
+
+    }
+}
+
